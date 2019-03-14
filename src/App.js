eb9bbd693937a2c119f6a909f6ff6c84c77681c2
Max Pikulin <trurl1000@gmail.com>
2019-03-14 17:22:30 +0000

    tweaks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import LinkedSliders from './linked-sliders/LinkedSliders';
+import Slider from './linked-sliders/Slider';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LinkedSliders />
+        {/* <LinkedSliders /> */}
+        <Slider />
       </div>
     );
   }
--- src/linked-sliders/Slider.js
+++ src/linked-sliders/Slider.js
@@ -1,32 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Slider = ({ value }) => {
+const Slider = ({ value = 600, onChange = (v) => console.log(v)}) => {
   const [range, useRange] = useState(0);
   
+  useEffect(() => {useRange(value)}, [value])
+  
+  const onRangeChange = (e) => {
+    useRange(+e.target.value);
+    onChange(range);
+  }
+
   return (
     <div>
       <input
         type='text'
         readOnly
-        value={
-          (() => {
-            if (value !== range) {
-              useRange(value);
-            }
-            return range;
-          })()
-        }
+        value={range}
       />
       <input
         type='range'
         defaultValue={value / 2}
         min='0'
         max={value}
-        onChange={(e) => {
-          if (range !== +e.target.value) {
-            useRange(+e.target.value);
-          }
-        }}
+        onChange={onRangeChange}
       />
       <input type='checkbox' />
     </div>
